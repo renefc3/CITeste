4ce7c45dcc7f85f7c6d536a40c9abd98eed6b9fa
Rene Felix Correa <renefc3@gmail.com>
2015-12-02 19:04:50 -0200

    Adicionado Site

---
+++ src/SolucaoTeste/WebApplicationTeste/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationTeste.Startup))]
+namespace WebApplicationTeste
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
